bf502aa1b9ab029da59d422b876b7b7991d99641
Roman Zimmermann <roman@more-onion.com>
2012-08-01 17:26:15 +0200

    Simpler english default messages

--- pgbar.tpl.php
+++ pgbar.tpl.php
@@ -3,24 +3,16 @@ $vars['!current'] = '<strong>' . number_format($current, 0) . '</strong>';
 $vars['!target'] = '<strong>' . number_format($target, 0) . '</strong>';
 $vars['!needed'] = number_format($target - $current, 0);
 
-$status_message = t('!current von !target erreicht.') . "\n";
+$intro_message = t('We need !target signatures.', $vars);
 if ($goal_reached) {
-	$intro_message  = t('Es wurden bereits !current für dieses Projekt gespendet.', $vars) . "\n";
-	$url = url('Danke füre ihre Spende.');
-	$link_text = t('Bitte helfen Sie mit, die fehlenden !needed zu erreichen.', $vars);
-	$intro_message .= "<a href=\"$url\">$link_text</a>\n";
+	$status_message = t("We've reached our goal!") . "\n";
 } else {
-	$intro_message  = t('Es wurden bereits !current für dieses Projekt gespendet.', $vars) . "\n";
-	$intro_message .= t('Finanzierungsbedarf: !target', $vars);
-	$url = url('spenden');
-	$link_text = t('Bitte helfen Sie mit, die fehlenden !needed zu erreichen.', $vars);
-	$intro_message .= "<a href=\"$url\">$link_text</a>\n";
-	$status_message .= t('100% wurden erreicht') . "\n";
+	$status_message = t('Already !current of !target signed the petition!') . "\n";
 }
 ?>
 <div class="pgbar-wrapper" data-pgbar-current="<?php print $current; ?>" data-pgbar-target="<?php print $target; ?>">
-  <p><?php $intro_message; ?></p>
+  <p><?php print $intro_message; ?></p>
   <div class="pgbar-bg"><div class="pgbar-current" style="width:<?php echo $percentage; ?>%"></div></div>
   <div class="pgbar-percent"><?php print number_format($percentage, 2) . '%'; ?></div>
-	<p><?php $status_message; ?></p> 
+	<p><?php print $status_message; ?></p> 
 </div>
