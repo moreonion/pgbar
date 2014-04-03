9bc024de234823f6b29422fab8eff8f5e90ff6ed
Roman Zimmermann <roman@more-onion.com>
2014-04-03 21:22:06 +0200

    Remove overly complicated query for sum of webform fields.

--- plugins/source/webform_field_sum.inc.php
+++ plugins/source/webform_field_sum.inc.php
@@ -29,26 +29,20 @@ class PgbarSourceWebformSum {
    */
   public function getValue($item) {
     $entity = $this->entity;
-    $q = db_select('node', 'n');
-    $q->addExpression('SUM(wsd.data)');
-    if (module_exists('variations')) {
-      $q->leftJoin('variations', 'v', "n.nid=v.entity_id AND v.entity_type='node'");
-      $q->leftJoin('variations', 'vn', "v.vid=vn.vid AND v.entity_type='node'");
-      $q->innerJoin('webform_component', 'wc', 'wc.nid = vn.entity_id OR (vn.entity_id IS NULL AND wc.nid=n.nid)');
-    }
-    else {
-      $q->innerJoin('webform_component', 'wc', 'n.nid=wc.nid');
-    }
-    $q->innerJoin('webform_submitted_data', 'wsd', 'wsd.nid=wc.nid AND wc.cid=wsd.cid')
-      ->where(
-        '(n.nid=:nid OR ((n.nid=:tnid OR n.tnid=:tnid) AND :tnid>0)) AND wc.form_key=:fkey',
-        array(
-          ':nid' => $entity->nid,
-          ':tnid' => $entity->tnid,
-          ':fkey' => $item['options']['source']['form_key'],
-        )
-      );
-    return $q->execute()->fetchField();
+    $sql = <<<EOSQL
+SELECT SUM(wsd.data)
+FROM {node} n
+  INNER JOIN {webform_component} wc ON n.nid=wc.nid
+  INNER JOIN {webform_submitted_data} wsd ON wsd.nid=wc.nid AND wc.cid=wsd.cid
+WHERE
+  (n.nid=:nid OR ((n.nid=:tnid OR n.tnid=:tnid) AND :tnid>0)) AND w.form_key=:fkey
+EOSQL;
+    $args = array(
+      ':nid' => $entity->nid,
+      ':tnid' => $entity->tnid,
+      ':fkey' => $item['options']['source']['form_key'],
+    );
+    return db_query($sql, $args)->fetchField();
   }
   /**
    * Build the configuration form for the field widget.
